e71eff2761a62f2f17c4c642e7b2428119439e12
Jake <jworsham9697@gmail.com>
2020-06-05 14:52:13 -0500

    CREATED FAVOIRTES VIEW

---
+++ API_Group_Project_Movie_GC_June2020/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Group_Project_Movie_GC_June2020.Models
+{
+    public class Movie
+    {
+    }
+}
