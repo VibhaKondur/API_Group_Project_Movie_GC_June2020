e8849ba1e57f09d6672339e35cd4c0f98fd666e9
LucasCarver <carver.lucas@gmail.com>
2020-06-06 03:07:45 -0400

    changed some controller methods

--- API_Group_Project_Movie_GC_June2020/Models/MovieDAL.cs
+++ API_Group_Project_Movie_GC_June2020/Models/MovieDAL.cs
@@ -20,15 +20,15 @@ namespace API_Group_Project_Movie_GC_June2020.Models
         public HttpClient GetClient()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://api.themoviedb.org/3");
+            client.BaseAddress = new Uri("https://api.themoviedb.org/");
             return client;
         }
 
         public async Task<Movie> GetMovieByTitleSearchJSON(string searchTitle, int i)
         {
             var client = GetClient();
-            var response = await client.GetAsync($"/search/movie?api_key={_APIKey}&query={searchTitle}");
-            string searchOutput = await response.Content.ReadAsStringAsync();
+            var response = await client.GetAsync($"/3/search/movie?api_key={_APIKey}&query={searchTitle}");
+            var searchOutput = await response.Content.ReadAsStringAsync();
             JObject json = JObject.Parse(searchOutput);
             List<JToken> modelData = json["results"].ToList();
             Movie movieObject = JsonConvert.DeserializeObject<Movie>(modelData[i].ToString());
@@ -38,9 +38,11 @@ namespace API_Group_Project_Movie_GC_June2020.Models
         public async Task<List<Movie>> GetMovieListByTitleSearch(string searchTitle)
         {
             List<Movie> ml = new List<Movie>();
+            Movie m;
             for (int i = 0; i < 200; i++)
             {
-                await ml.Add((Movie)GetMovieByTitleSearchJSON(searchTitle, i));
+                m = await GetMovieByTitleSearchJSON(searchTitle, i);
+                ml.Add(m);
             }
             return ml;
         }
@@ -49,7 +51,7 @@ namespace API_Group_Project_Movie_GC_June2020.Models
         //{
         //}
 
-        //public async Task<MovieDetail> GetMovieDetailForFavorites(int id)
+        //public async Task<MovieDetail> GetMovieDetailForFavorites(int movie_id)
         //{
         //    var client = GetClient();
         //    var response = await client.GetAsync($"/movie/{id}?api_key={_APIKey}");
