0c32b46aa1f4a18b1b2bd7c2bcddf0e0500d7ef5
Christina Lambert <clambert0615@gmail.com>
2020-06-08 10:41:21 -0400

    merged the views, controllers, and dal

--- API_Group_Project_Movie_GC_June2020/Controllers/MovieController.cs
+++ API_Group_Project_Movie_GC_June2020/Controllers/MovieController.cs
@@ -1,44 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API_Group_Project_Movie_GC_June2020.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 namespace API_Group_Project_Movie_GC_June2020.Controllers
 {
+    [Authorize]
     public class MovieController : Controller
     {
+        private readonly FilmsDbContext _context;
+
         private readonly string _APIKey;
 
         private readonly MovieDAL _movieDAL;
 
-        public MovieController(IConfiguration configuration)
+        public MovieController(IConfiguration configuration, FilmsDbContext context)
         {
             _APIKey = configuration.GetSection("APIKey")["MovieAPI"];
             _movieDAL = new MovieDAL(_APIKey);
+            _context = context;
         }
+
         public IActionResult SearchIndex()
         {
             return View();
         }
 
+        public IActionResult AddToFavorite(int id)
+        {
+            Favorites favorite = new Favorites();
+            favorite.ApiId = id;
+            favorite.UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if(_context.Favorites.Where(x => (x.ApiId == id) && (x.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value)).ToList().Count > 0)
+            {
+                return RedirectToAction("Favorites");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Favorites.Add(favorite);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Favorites");/*, new { id = favorite.Id});*/
+        }
 
-        //public IActionResult SearchResult(string searchTitle)
-        //{
-        //    List<Movie> ml = new List<Movie>();
-        //    ml = _movieDAL.GetMovieListByTitleSearch(searchTitle);
-        //    return RedirectToAction("Results", ml);
-        //}
-
-        [HttpPost]
-        public IActionResult Results(string searchTitle)
+        public async Task<IActionResult> Favorites()
         {
             List<MovieDetail> ml = new List<MovieDetail>();
-            ml = _movieDAL.GetMovieListByTitleSearch(searchTitle);
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var favoritesOfUser = _context.Favorites.Where(x => x.UserId == id).ToList();
+            ml = await _movieDAL.GetFavoriteMoviesList(favoritesOfUser);
             return View(ml);
         }
 
+        public IActionResult RemoveFilm(int id)
+        {
+            string uid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            Favorites found = _context.Favorites.FirstOrDefault(x => (x.ApiId == id) && (x.UserId == uid));
+            if (found != null)
+            {
+                _context.Favorites.Remove(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Favorites");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Results(string searchTitle)
+        {
+            List<MovieDetail> ml = await _movieDAL.GetMovieListByTitleSearch(searchTitle);
+            return View(ml);
+        }
     }
 }
--- API_Group_Project_Movie_GC_June2020/Models/MovieDAL.cs
+++ API_Group_Project_Movie_GC_June2020/Models/MovieDAL.cs
@@ -19,54 +19,60 @@ namespace API_Group_Project_Movie_GC_June2020.Models
             _APIKey = APIKey;
         }
 
-        public string GetBaseUrl()
+        public HttpClient GetClient()
         {
-            return "https://api.themoviedb.org/";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri($"https://api.themoviedb.org/");
+            return client;
         }
 
-        public MovieDetail GetMovieByTitleSearchJSON(string searchTitle, int i)
+        public async Task<List<MovieDetail>> GetMovieListByTitleSearch(string searchTitle)
         {
-            string url = GetBaseUrl() + $"3/search/movie?api_key={_APIKey}&query={searchTitle}";
-            HttpWebRequest request = WebRequest.CreateHttp(url);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader rd = new StreamReader(response.GetResponseStream());
-            string output = rd.ReadToEnd();
-            JObject json = JObject.Parse(output);
-            List<JToken> modelData = json["results"].ToList();
-            MovieDetail movieObject = JsonConvert.DeserializeObject<MovieDetail>(modelData[i].ToString());
-            url = GetBaseUrl() + $"3/movie/{movieObject.id}?api_key={_APIKey}";
-            request = WebRequest.CreateHttp(url);
-            response = (HttpWebResponse)request.GetResponse();
-            rd = new StreamReader(response.GetResponseStream());
-            output = rd.ReadToEnd();
-            json = JObject.Parse(output);
-            JToken runtimeFromJson = json["runtime"];
-            movieObject.runtime = JsonConvert.DeserializeObject<int>(runtimeFromJson.ToString());
+            var client = GetClient();
+            var response = await client.GetAsync($"/3/search/movie?api_key={_APIKey}&query={searchTitle}");
+            var movieJson = await response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(movieJson);
+            JToken modelData = json["results"];
+            List<MovieDetail> movieObject = JsonConvert.DeserializeObject<List<MovieDetail>>(modelData.ToString());
+            foreach (MovieDetail m in movieObject)
+            {
+                var response2 = await client.GetAsync($"/3/movie/{m.id}?api_key={_APIKey}");
+                var movieJson2 = await response2.Content.ReadAsStringAsync();
+                JObject json2 = JObject.Parse(movieJson2);
+                JToken runtimeFromJson = json2["runtime"];
+                if (!int.TryParse(runtimeFromJson.ToString(), out int i))
+                {
+                    m.runtime = 0;
+                }
+                else
+                {
+                    m.runtime = JsonConvert.DeserializeObject<int>(runtimeFromJson.ToString());
+                }
+            }
+            foreach (MovieDetail m in movieObject)
+            {
+                if (m.release_date == null)
+                {
+                    m.release_date = "Release date not found";
+                }
+            }
             return movieObject;
         }
 
-        public List<MovieDetail> GetMovieListByTitleSearch(string searchTitle)
+        public async Task<List<MovieDetail>> GetFavoriteMoviesList(List<Favorites> fl)
         {
             List<MovieDetail> ml = new List<MovieDetail>();
-            MovieDetail m;
-            for (int i = 0; i < 2000; i++)
+            var client = GetClient();
+            foreach (Favorites f in fl)
             {
-                try
-                {
-                    m = GetMovieByTitleSearchJSON(searchTitle, i);
-                    ml.Add(m);
-                }
-                catch(ArgumentOutOfRangeException e)
-                {
-                    i = 2000;
-                }
+                var response = await client.GetAsync($"/3/movie/{f.ApiId}?api_key={_APIKey}");
+                var movieJson = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(movieJson);
+                MovieDetail movie = JsonConvert.DeserializeObject<MovieDetail>(json.ToString());
+                ml.Add(movie);
             }
             return ml;
         }
-
-        //public async Task<List<MovieDetail>> GetFavoritesList()
-        //{
-        //}
 
         //public async Task<MovieDetail> GetMovieDetailForFavorites(int movie_id)
         //{
